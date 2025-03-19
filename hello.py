16e24c18068054f6c91085f7a02070ec411eaa3a
bmahin <bobbymahin@yahoo.com>
2025-03-18 22:17:50 -0700

    testing webpage opening functionality

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www2.1010data.com/prime-17.35/")
+
+time.sleep(5)
+
+driver.quit()
