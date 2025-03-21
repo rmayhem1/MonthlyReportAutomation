35636393bd6b230a390c135d48ee53089d4b97bd
rmayhem1 <robmahin.dev@gmail.com>
2025-03-21 11:30:14 -0700

    username/password input and login button

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import os
 import time
+
+load_dotenv() #import .env file contents
+
+username = os.getenv("USERINFO")
+password = os.getenv("PASSWORD")
+url = os.getenv("URL")
 
 driver = webdriver.Chrome()
 
-driver.get("https://www2.1010data.com/prime-17.35/")
+driver.get(url)
 
-time.sleep(5)
+time.sleep(5) #wait until elements are loaded on page (CHANGE TO WebDriverWait in future)
 
-driver.quit()
+#grab login/password login boxes via XPATH
+username_field = driver.find_element(By.XPATH, '//*[@id="uid"]') 
+password_field = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div[1]/div[2]/form/div[4]/input') #had to use full XPATH bc shortened was not working
+
+username_field.send_keys(username)
+password_field.send_keys(password)
+
+login_button = driver.find_element(By.XPATH, '//*[@id="submit_button"]')
+login_button.click()
+
+#add long wait (WebDriver Explicit Wait) here bc login times are atrocious
+
